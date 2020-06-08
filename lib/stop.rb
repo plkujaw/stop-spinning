1dbea413dc1fa420288ed4909ca70250daf9ff34
plkujaw <jawor.jakub@gmail.com>
2020-06-08 12:07:16 +0100

    refactored

--- lib/stop.rb
+++ lib/stop.rb
@@ -1,10 +1,4 @@
 def spinWords(string)
-  result = string.split(" ").map do |word|
-    if word.length >= 5
-      word.reverse
-    else
-      word
-    end
-  end
-  result.join(" ")
+string.split(" ").map { }word| if word.length >= 5 ? word.reverse : word }
+
 end
