ebdf9222f716ba4b0866092b2115ab8e1957f68e
plkujaw <jawor.jakub@gmail.com>
2020-06-08 11:33:52 +0100

    add and pass first test

--- spec/stop_spec.rb
+++ spec/stop_spec.rb
@@ -3,5 +3,6 @@ require "stop"
 describe "#spinWords" do
   it "returns words shorter than 5 unchanged" do
     expect(spinWords("test")).to eq("test")
+    expect(spinWords("dont spin this")).to eq("dont spin this")
   end
 end
