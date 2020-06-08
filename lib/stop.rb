59068a03eb0d74f8a5985517d408322b7f007b9b
plkujaw <jawor.jakub@gmail.com>
2020-06-08 12:14:30 +0100

    added a code before refactoring as a comment to compare

--- lib/stop.rb
+++ lib/stop.rb
@@ -1,3 +1,15 @@
 def spinWords(string)
   string.split.map { |word| word.length >= 5 ? word.reverse : word }.join(" ")
 end
+
+# def spinWords(string)
+#   result = []
+#   string.split(" ").each do |word|
+#     if word.length >= 5
+#       result << word.reverse
+#     else
+#       result << word
+#     end
+#   end
+#   result.join(" ")
+# end
